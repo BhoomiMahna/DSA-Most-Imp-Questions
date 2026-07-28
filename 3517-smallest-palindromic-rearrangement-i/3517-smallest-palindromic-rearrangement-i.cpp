afc90640d8441821eec1d5e6c92efadb3f5249b6
BhoomiMahna <mahnabhoomi@gmail.com>
2026-07-28 09:00:57 +0530

    Time: 12 ms (97.58%), Space: 60.9 MB (80%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
     string smallestPalindrome(string s) {
-        /* aabbbcc
-         ab
-        */
-        sort(s.begin(),s.end());
-        map<char,int>mp;
-        for(int i=0;i<s.size();i++){
-            mp[s[i]]++;
-        }
-        string ans;
-        vector<char>odd;
-        for(auto it:mp){
-            int div=it.second/2;
-            int rem=it.second%2;
-            
-            while(div--){
-                ans+=it.first;
+        vector<int> freq(26, 0);
+        for (char c : s)
+            freq[c-'a']++;
+
+        int n = s.size();
+        string ans(n,' ');
+
+        int left = 0, right = n - 1;
+
+        for (int i = 0; i < 26; i++) {
+            while (freq[i] >= 2) {
+                ans[left++] = char('a' + i);
+                ans[right--] = char('a' + i);
+                freq[i] -= 2;
             }
-            if(rem!=0){
-                odd.push_back(it.first);
+
+            if (freq[i] == 1) {
+                ans[s.size() / 2] = char('a' + i);
             }
         }
-        string res=ans;
-        reverse(ans.begin(),ans.end());
-        if(!odd.empty())res+=odd[0];
-        res+=ans;
-        return res;
+
+        return ans;
     }
 };
